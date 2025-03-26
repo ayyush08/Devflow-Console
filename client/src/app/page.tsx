93cbd21b082b3041da384058f5c277b992619323
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-03-27 03:46:17 +0530

    feat: update app metadata and enhance page layout with user guidance

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -36,8 +36,12 @@ const MetricsDashboard = () => {
 
 
     return (
-        <div className='flex flex-col justify-center items-center px-4 min-h-screen'>
-
+        <div className='flex flex-col justify-center items-center px-4 min-h-screen relative'>
+            <p className='text-lg w-full text-cyan-200 absolute text-center bottom-0  p-2'>
+            {"NOTE: If you see 'Failed to fetch data,' our server might be taking a bit longer to start. 🚀 It usually takes about 1-1.5 minutes. Please refresh and try again shortly! 🔄"}
+            </p>
+            <h1 className='bg-gradient-to-b text-transparent bg-clip-text from-cyan-500 to-red-400 absolute top-0 left-0 mx-auto p-5 text-4xl font-extrabold'>
+                Devflow-Console</h1>
             {repo === "" ? (
                 <div className="flex flex-col justify-center items-center px-4">
                     <h2 className="text-lg md:text-2xl lg:text-6xl font-sans bg-gradient-to-b text-transparent bg-clip-text from-orange-500 to-teal-400 py-2 md:py-10 font-bold">
